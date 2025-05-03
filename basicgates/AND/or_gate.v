d0c9f740b6465982066a544312ec8187ab4fbdb3
NIMBAGALLUVISHNU <141856162+NIMBAGALLUVISHNU@users.noreply.github.com>
2025-05-03 23:03:43 +0530

    Add files via upload

---
+++ basicgates/AND/or_gate.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02.05.2025 10:38:08
+// Design Name: 
+// Module Name: or_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module or_gate(a,b,y);
+input a,b;
+output y;
+or (y,a,b);
+endmodule
